499e828e85c6409ca6dbef2e8f5e3e05016b52e5
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2022-09-10 17:37:27 +0530

    Add sitemap extension

--- apps/vipatra.in/astro.config.mjs
+++ apps/vipatra.in/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import svelte from "@astrojs/svelte";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), svelte()]
+  site: 'https://vipatra.in',
+  integrations: [tailwind(), sitemap(), svelte(), image()]
 });
