d186c2c1d4cb4ef716beac8402e80570c66fceb1
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2022-09-14 18:42:14 +0530

    Update Astro & remove image extension

--- apps/vipatra.in/astro.config.mjs
+++ apps/vipatra.in/astro.config.mjs
@@ -3,10 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 
-import image from "@astrojs/image";
-
-// https://astro.build/config
 export default defineConfig({
   site: 'https://vipatra.in',
-  integrations: [tailwind(), sitemap(), svelte(), image()]
+  integrations: [tailwind(), sitemap(), svelte()]
 });
