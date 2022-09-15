8439f27ff39f1547129c63afda8a81be8f34ab43
VIGNESH <60360863+vigneshksaithal@users.noreply.github.com>
2022-09-15 23:38:06 +0530

    Create dashboard

---
+++ apps/dashboard/src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDJ0Zla5Z_1lDFql9N2gcCp16piWqeypLI",
+    authDomain: "vipatra-menu.firebaseapp.com",
+    projectId: "vipatra-menu",
+    storageBucket: "vipatra-menu.appspot.com",
+    messagingSenderId: "872885329675",
+    appId: "1:872885329675:web:767a38b12a7d2b4261cc0e",
+    measurementId: "G-BEQ9BGM55T"
+};
+
+const app = initializeApp(firebaseConfig);
---
+++ apps/dashboard/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3B82F6',
+      }
+    },
+  },
+  plugins: [],
+}
